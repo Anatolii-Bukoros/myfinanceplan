ecc9225dc8ff135df9a1675bd398cd42c4ed8535
Anatolii Bukoros <108886627+Anatolii-Bukoros@users.noreply.github.com>
2025-05-23 11:06:15 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Мій фінансовий план</h1>
+      <p>Тут буде форма для введення балансу та витрат</p>
+    </div>
+  );
+}
+
+export default App;
