735519f04f15a4d6d14d34017608329e15bcbb27
Anatolii Bukoros <108886627+Anatolii-Bukoros@users.noreply.github.com>
2025-05-23 12:25:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
 function App() {
-  const [form, setForm] = useState({
-    pump: "",
-    privat: "",
-    monoWhite: "",
-    monoBlack: "",
-    cash: "",
-    bolt: "",
-    fuel: "",
-    mileage: ""
-  });
-
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-
   return (
     <div className="App">
       <h1>Мій фінансовий план</h1>
 
-      <div className="form">
-        <label>Картка PUMP: <input name="pump" value={form.pump} onChange={handleChange} /></label>
-        <label>Privat24: <input name="privat" value={form.privat} onChange={handleChange} /></label>
-        <label>Monobank (біла): <input name="monoWhite" value={form.monoWhite} onChange={handleChange} /></label>
-        <label>Monobank (чорна): <input name="monoBlack" value={form.monoBlack} onChange={handleChange} /></label>
-        <label>Готівка: <input name="cash" value={form.cash} onChange={handleChange} /></label>
-        <label>Bolt: <input name="bolt" value={form.bolt} onChange={handleChange} /></label>
-        <label>Літри в баку: <input name="fuel" value={form.fuel} onChange={handleChange} /></label>
-        <label>Пробіг (км): <input name="mileage" value={form.mileage} onChange={handleChange} /></label>
+      <div className="logos">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/3/3e/Monobank_logo.svg" alt="Monobank" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/6e/PrivatBank-corporate-logo-latina.png" alt="PrivatBank" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/3/3d/Pumb_logo.svg" alt="PUMB" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/2c/Bolt_logo.png" alt="Bolt" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Uklon_logo.jpg" alt="Uklon" />
+        <img src="https://cdn-icons-png.flaticon.com/512/1041/1041916.png" alt="Cash" />
       </div>
+
+      <p>Тут буде форма для введення балансу та витрат</p>
     </div>
   );
 }
