2f1fbf2353247d5ea58af1f411d160e634fd2207
Anatolii Bukoros <108886627+Anatolii-Bukoros@users.noreply.github.com>
2025-05-23 12:34:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
       <h1>Мій фінансовий план</h1>
 
       <div className="logos">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/3/3e/Monobank_logo.svg" alt="Monobank" />
-        <img src="https://upload.wikimedia.org/wikipedia/commons/6/6e/PrivatBank-corporate-logo-latina.png" alt="PrivatBank" />
-        <img src="https://upload.wikimedia.org/wikipedia/commons/3/3d/Pumb_logo.svg" alt="PUMB" />
-        <img src="https://upload.wikimedia.org/wikipedia/commons/2/2c/Bolt_logo.png" alt="Bolt" />
-        <img src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Uklon_logo.jpg" alt="Uklon" />
-        <img src="https://cdn-icons-png.flaticon.com/512/1041/1041916.png" alt="Cash" />
+        <img src="/logos/mono.png" alt="Monobank" />
+        <img src="/logos/privat.png" alt="PrivatBank" />
+        <img src="/logos/pumb.png" alt="PUMB" />
+        <img src="/logos/bolt.png" alt="Bolt" />
+        <img src="/logos/uklon.png" alt="Uklon" />
+        <img src="/logos/cash.png" alt="Cash" />
       </div>
 
       <p>Тут буде форма для введення балансу та витрат</p>
